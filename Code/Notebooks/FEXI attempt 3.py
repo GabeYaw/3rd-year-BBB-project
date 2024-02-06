5e2098acf7f072c7a60a1662ec536b160e74d7c1
Schedule7711 <zcemgmy@ucl.ac.uk>
2024-02-06 12:29:00 +0000

    exported notebook to python and organising cells

---
+++ Code/Notebooks/FEXI attempt 3.py
@@ -0,0 +1,517 @@
+# %% ### Imports
+
+from itertools import product
+import matplotlib.pyplot as plt
+import nibabel as nib
+import numpy as np
+import scipy.stats
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as utils
+from dmipy.core.acquisition_scheme import acquisition_scheme_from_bvalues
+from dmipy.core.modeling_framework import MultiCompartmentSphericalMeanModel
+from dmipy.signal_models import cylinder_models, gaussian_models, sphere_models
+from scipy.io import savemat
+from scipy.special import erf
+from tqdm import tqdm
+
+# %% Simulate Signal
+def sim_sig_np(bf,be,tm,adc,sigma,axr,nvox):
+    be_tiled = np.tile(be,(nvox,1))
+    bf_tiled = np.tile(bf,(nvox,1))
+    tm_tiled = np.tile(tm,(nvox,1))
+
+    adc_tiled = np.transpose(np.tile(adc,(np.size(tm),1)))
+    sigma_tiled = np.transpose(np.tile(sigma,(np.size(tm),1)))
+    axr_tiled = np.transpose(np.tile(axr,(np.size(tm),1)))
+
+    tm_tiled[(tm_tiled == np.min(tm_tiled)) & (bf_tiled == 0)] = np.inf
+
+    adc_prime_tiled = adc_tiled * (1 - sigma_tiled* np.exp(-tm_tiled*axr_tiled))
+    normalised_signal_tiled = np.exp(-adc_prime_tiled * be_tiled)
+
+    return normalised_signal_tiled, adc_prime_tiled
+
+def sim_sig_np_1_vox(bf,be,tm,adc,sigma,axr):
+    adc_tiled = np.transpose(np.tile(adc,(np.size(tm),1)))
+    sigma_tiled = np.transpose(np.tile(sigma,(np.size(tm),1)))
+    axr_tiled = np.transpose(np.tile(axr,(np.size(tm),1)))
+
+    tm[(tm == np.min(tm)) & (bf == 0)] = np.inf
+
+    adc_prime = adc_tiled * (1 - sigma_tiled* np.exp(-tm*axr_tiled))
+    normalised_signal = np.exp(-adc_prime * be)
+    
+    return normalised_signal, adc_prime
+
+def sim_sig_pytorch_new(bf,be,tm,adc,sigma,axr,nvox):
+    be_tiled = torch.tile(be,(nvox,1))
+    bf_tiled = torch.tile(bf,(nvox,1))
+    tm_tiled = torch.tile(tm,(nvox,1))
+    
+    """adc_tiled = torch.tile(adc,(tm.shape[0],1)).t()
+    sigma_tiled = torch.tile(sigma,(tm.shape[0],1)).t()
+    axr_tiled = torch.tile(axr,(tm.shape[0],1)).t()"""
+
+    adc_tiled = torch.tile(adc,(1,tm.shape[0]))
+    sigma_tiled = torch.tile(sigma,(1,tm.shape[0]))
+    axr_tiled = torch.tile(axr,(1,tm.shape[0]))
+
+    tm_tiled[(tm_tiled == torch.min(tm_tiled)) & (bf_tiled == 0)] = torch.inf
+
+    """print("be: ", be_tiled.shape)
+    print("bf: ", bf_tiled.shape)
+    print("tm: ", tm_tiled.shape)
+    print("adc: ", adc_tiled.shape)
+    print("sigma: ", sigma_tiled.shape)
+    print("axr: ", axr_tiled.shape)"""
+
+
+    adc_prime_tiled = adc_tiled * (1 - sigma_tiled*torch.exp(-tm_tiled*axr_tiled))
+    normalised_signal_tiled = torch.exp(-adc_prime_tiled * be_tiled)
+    
+    return normalised_signal_tiled, adc_prime_tiled
+
+# %% Initial variables.
+
+nvox = 1000 # number of voxels to simulate
+
+bf = np.array([0, 0, 250, 250, 250, 250, 250, 250]) * 1e-3   # filter b-values [ms/um2]
+be = np.array([0, 250, 0, 250, 0, 250, 0, 250]) * 1e-3       # encoding b-values [ms/um2]
+tm = np.array([20, 20, 20, 20, 200, 200, 400, 400], dtype=np.float32) * 1e-3 # mixing time [s]
+
+adc_lb = 0.1        #[um2/ms]
+adc_ub = 3.5        #[um2/ms]
+sig_lb = 0          #[a.u.]
+sig_ub = 1          #[a.u.]
+axr_lb = 0.1        #[s-1]
+
+axr_lb = 1        #[s-1]
+
+axr_ub = 20         #[s-1]
+
+#consider doing in si units
+
+limits = np.array([[adc_lb, adc_ub], [sig_lb, sig_ub] , [axr_lb, axr_ub]])
+
+adc_init = (adc_lb + adc_ub) / 2 #[um2/ms]
+sig_init = (sig_lb + sig_ub) / 2 #[a.u.]
+axr_init = (axr_lb + axr_ub) / 2 #[ms-1]
+
+num_inits = 5
+
+# Create equally spaced arrays for each parameter
+# remove first and last values which are on the "face of the cube"
+adc_inits = np.linspace(adc_lb, adc_ub, num_inits)[1:-1]
+sig_inits = np.linspace(sig_lb, sig_ub, num_inits)[1:-1]
+axr_inits = np.linspace(axr_lb, axr_ub, num_inits)[1:-1]
+
+# Generate all permutations of combinations
+all_inits = list(product(adc_inits, sig_inits, axr_inits))
+
+# Convert the list of tuples to a NumPy array
+all_inits = np.array(all_inits)
+
+sim_adc = np.random.uniform(adc_lb,adc_ub,nvox)                 # ADC, simulated [um2/ms]
+sim_sigma = np.random.uniform(sig_lb,sig_ub,nvox)               # sigma, simulated [a.u.]
+sim_axr = np.random.uniform(axr_lb,axr_ub,nvox)                 # AXR, simulated [s-1]
+
+sim_E_vox, sim_adc_prime = sim_sig_np(bf,be,tm,sim_adc,sim_sigma,sim_axr,nvox)
+
+
+#a_test1,a_test2 = sim_sig_pytorch(bf,be,tm,sim_adc,sim_sigma,sim_axr,len(sim_axr))
+
+
+# %% Histogram plots
+n_bins=20
+#make 200 and voxels 100,000 to see adc prime dropoff
+fig, axs = plt.subplots(1, 3, sharey=True,figsize=(15, 5))
+axs[0].hist(sim_adc, bins=n_bins)
+axs[1].hist(sim_sigma, bins=n_bins)
+axs[2].hist(sim_axr, bins=n_bins)
+axs[0].set_title('ADC Histogram ')
+axs[0].set_xlabel('ADC Values [um^2/ms]')
+axs[1].set_title('Sigma Histogram')
+axs[1].set_xlabel('Sigma Values [arbitrary units]')
+axs[2].set_title('AXR Histogram')
+axs[2].set_xlabel('AXR Values [ms-1]');
+
+
+fig, axs = plt.subplots(1, 3,figsize=(15, 5))
+axs[0].hist(sim_E_vox.flatten(), bins=n_bins)
+axs[0].set_title('Signal Histogram ')
+axs[0].set_xlabel('Signal Values [units?]')
+axs[1].hist(sim_E_vox[sim_E_vox != 1].flatten(), bins=n_bins)
+axs[1].set_title('Signal Histogram with ones removed')
+axs[1].set_xlabel('Signal Values [units?]')
+axs[2].hist(sim_adc_prime.flatten(), bins=n_bins)
+axs[2].set_title('ADC prime Histogram')
+axs[2].set_xlabel('ADC prime Values [units?]');
+
+
+# %% Plotting b-value against normalised signal
+
+plt.plot([be[0], be[1]], [sim_E_vox[0,0], sim_E_vox[0,1]], 'bo-')
+plt.annotate(tm[0], (be[1], sim_E_vox[0, 1]), textcoords="offset points", xytext=(20,5), ha='center')
+
+plt.plot([be[2], be[3]], [sim_E_vox[0,2], sim_E_vox[0,3]], 'go-')
+plt.annotate(tm[2], (be[3], sim_E_vox[0, 3]), textcoords="offset points", xytext=(20,5), ha='center')
+
+plt.plot([be[4], be[5]], [sim_E_vox[0,4], sim_E_vox[0,5]], 'ko-')
+plt.annotate(tm[4], (be[5], sim_E_vox[0, 5]), textcoords="offset points", xytext=(20,5), ha='center')
+
+plt.plot([be[6], be[7]], [sim_E_vox[0,6], sim_E_vox[0,7]], 'mo-')
+plt.annotate(tm[6], (be[7], sim_E_vox[0, 7]), textcoords="offset points", xytext=(20,5), ha='center')
+
+
+#plt.title('Scatter plot with 4 lines')
+plt.xlabel('Encoding block b-value [m2/s]')
+#are units correct
+plt.ylabel('Normalised Signal (sum of the magnetisations)')
+#unit?
+plt.grid(True)
+plt.show()
+
+
+# %% Least squares fit
+def sse_adc_prime_1_vox(variables_to_optimize, tm, bf, be, smeas):
+    # For the signal from 1 voxel.
+    adc_est, sigma_est, axr_est = variables_to_optimize
+    _ , adc_tm_fit = sim_sig_np_1_vox(bf,be,tm,adc_est,sigma_est,axr_est)
+
+    bf_tm = np.column_stack((bf.flatten(), tm.flatten()))
+
+    # Find unique rows and corresponding indices
+    univols, univols_indices = np.unique(bf_tm, axis=0, return_index=True)
+
+    nsf = univols.shape[0]
+
+    ix1 = np.where((np.sum(univols[:, None, :] == bf_tm, axis=2) == 2) & (be == 0))[1]
+    ix2 = np.where((np.sum(univols[:, None, :] == bf_tm, axis=2) == 2) & (be > 0))[1]
+    
+    #this line is hardcoded
+    smeas = smeas.reshape(8)
+
+    adc_tm_calc = -1 / (be[ix2] - be[ix1]) * np.log(smeas[ix2] / smeas[ix1])
+
+    #this line is hardcoded
+    adc_tm_fit = adc_tm_fit[:, ::2]
+
+    sse = np.sum((adc_tm_calc - adc_tm_fit) ** 2)
+    return sse
+
+
+# %%
+def callback(xk):
+    print('Estimates of ADC, sigma, AXR:    ', xk)
+
+# Define the bounds for adc, sigma, axr
+bounds = tuple(map(tuple, limits.tolist())) #this line seems messy
+
+NLLS_adc_all = np.empty(shape=(0,))
+NLLS_sigma_all = np.empty(shape=(0,))
+NLLS_axr_all = np.empty(shape=(0,))
+    
+NLLS_adc_prime_all = np.empty(shape=(nvox,8))
+NLLS_E_vox_all = np.empty(shape= (nvox,8))
+
+sses = np.array([])
+for current_vox in range(nvox):
+    #reset best for each voxel
+    best_sse = 1
+
+    #extract relevant info for current voxel
+    cur_E_vox = sim_E_vox[current_vox,:]
+    cur_adc_prime = sim_adc_prime[current_vox,:]
+
+    cur_adc = sim_adc[current_vox]
+    cur_sigma = sim_sigma[current_vox]
+    cur_axr = sim_axr[current_vox]
+
+    additional_args_1_vox = (tm, bf, be, cur_E_vox) 
+
+    for combination in range(all_inits.shape[0]):
+        inits = all_inits[combination,:]
+        
+        result_1_vox = scipy.optimize.minimize(sse_adc_prime_1_vox, inits, args=additional_args_1_vox, bounds=bounds)
+
+        if result_1_vox.fun < best_sse:
+            best_sse = result_1_vox.fun
+            NLLS_cur_adc, NLLS_cur_sigma, NLLS_cur_axr = result_1_vox.x
+    
+    sses = np.append(sses,best_sse)
+    # note the 1 instead of nvox, because it is for 1 voxel 
+    NLLS_cur_E_vox, NLLS_cur_adc_prime = sim_sig_np_1_vox(bf,be,tm,NLLS_cur_adc, NLLS_cur_sigma, NLLS_cur_axr)
+
+    NLLS_adc_all = np.append(NLLS_adc_all, NLLS_cur_adc)
+    NLLS_sigma_all = np.append(NLLS_sigma_all, NLLS_cur_sigma)
+    NLLS_axr_all = np.append(NLLS_axr_all, NLLS_cur_axr)
+    
+    NLLS_adc_prime_all[current_vox,:] = NLLS_cur_adc_prime
+    NLLS_E_vox_all[current_vox,:] = NLLS_cur_E_vox
+
+# %% NLLS Plots
+#Debugging sse and seeing how large it is for the worst values. 
+sses.sort()
+sses_descending = sses[::-1]
+
+
+"""MAYBE ADD LOSS PER 'EPOCH' FOR NLLS
+plt.figure()
+plt.plot(range(1, len(loss_progress) + 1), loss_progress, marker='o', linestyle='-')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Loss per Epoch')
+plt.grid(True)
+plt.show()"""
+
+
+#plotting the sse
+plt.figure()
+plt.hist(sses.flatten(), bins=200)
+
+plt.figure()
+# for first voxel
+plt.scatter(be, sim_E_vox[0,:], label='simulated')
+plt.scatter(be, NLLS_E_vox_all[0,:], label='predicted')
+plt.xlabel("be")
+plt.ylabel("tm")
+plt.legend()
+
+# plot scatter plots to analyse correlation of predicted free params against ground truth
+plt.figure()
+
+param_sim = [sim_adc, sim_sigma, sim_axr]
+param_pred = [NLLS_adc_all, NLLS_sigma_all, NLLS_axr_all]
+param_name = ['ADC', 'Sigma', 'AXR']
+
+rvals = []
+
+for i,_ in enumerate(param_sim):
+    plt.rcParams['font.size'] = '16'
+    plt.scatter(param_sim[i], param_pred[i], s=2, c='navy')
+    plt.xlabel(param_name[i] + ' Ground Truth')
+    plt.ylabel(param_name[i] + ' Prediction')
+    #check what line below does. Commented out because it gave an error when using 1 voxel
+    #rvals.append(scipy.stats.pearsonr(np.squeeze(param_sim[i]), np.squeeze(param_pred[i])))
+    plt.tight_layout
+    plt.show()
+
+print(rvals)
+
+
+# %% Creating the neural network
+
+class Net(nn.Module): # this is the neural network
+    #defining the init and foward pass functions. 
+
+    def __init__(self,be,bf,tm,nparams,limits):
+        super(Net, self).__init__()
+
+        self.be = be
+        self.bf = bf
+        self.tm = tm
+        self.limits = limits
+
+        #defining the layers that we want. 
+        # 3 layers with no. of be nodes. 
+        self.layers = nn.ModuleList()
+        for i in range(3): # 3 fully connected hidden layers
+            self.layers.extend([nn.Linear(len(be), len(be)), nn.PReLU()])
+            #https://pytorch.org/docs/stable/generated/torch.nn.PReLU.html
+        self.encoder = nn.Sequential(*self.layers, nn.Linear(len(be), nparams))
+
+    def forward(self, E_vox):
+
+        params = torch.nn.functional.softplus(self.encoder(E_vox))
+        #running a forward pass through the network
+
+        #SoftPlus is a smooth approximation to the ReLU function and can be used to constrain the output of a machine to always be positive
+        #params contains batch_size x nparams outputs, so each row is adc, sigma and axr.
+
+        #unsqueeze adds an additional dimension. 
+        #parameter constraints from Elizabeth matlab 
+
+        adc = torch.clamp(params[:, 0].unsqueeze(1), min=limits[0,0], max=limits[0,1])
+        sigma = torch.clamp(params[:, 1].unsqueeze(1), min=limits[1,0], max=limits[1,1])
+        axr = torch.clamp(params[:, 2].unsqueeze(1), min=limits[2,0], max=limits[2,1])
+        
+        self.tm[(self.tm == torch.min(self.tm)) & (self.bf == 0)] = float('inf')
+
+        adc_prime = adc * (1 - sigma * torch.exp(-tm * axr))
+        E_vox = torch.exp(-adc_prime * be)
+
+        """print("tm:", self.tm.shape)
+        print("be:", self.be.shape)
+        print("bf:", self.bf.shape)
+        print("adc:", adc.shape)
+        print("sigma:", sigma.shape)
+        print("axr:", axr.shape)
+        print("evox:", E_vox.shape)"""
+
+        print("self.encoder(E_vox)", self.encoder(E_vox)[0,:])
+        print("params:", params[0,:])
+        print("adc:", adc)
+        print("sigma:", sigma)
+        print("axr:", axr)
+        print("evox:", E_vox)
+
+        return E_vox, adc_prime, adc, sigma, axr
+
+
+# %% NN continued
+# define network
+nparams = 3
+#because of adc, sigma and axr
+
+#converting numpy arrays to pytorch tensors. 
+#be = torch.tensor(be)
+#bf = torch.tensor(bf)
+#tm = torch.tensor(tm)
+be = torch.tensor(be, dtype=torch.float32)
+bf = torch.tensor(bf, dtype=torch.float32)
+tm = torch.tensor(tm, dtype=torch.float32)
+
+batch_size = 128
+
+#initilise network
+net = Net(be, bf, tm, nparams,limits)
+
+#create batch queues for data
+#// means divide and round down. 
+num_batches = len(sim_E_vox) // batch_size
+
+#import the sim_E_vox array into the dataloader
+#drop_last ignores the last batch if it is the wrong size. 
+#num_workers is about performance. 
+
+trainloader = utils.DataLoader(torch.from_numpy(sim_E_vox.astype(np.float32)),
+                                batch_size = batch_size, 
+                                shuffle = True,
+                                num_workers = 0, #was 2 previously
+                                drop_last = True)
+
+# loss function and optimizer
+#choosing which loss function to use. 
+#not sure what the optmizer is
+criterion = nn.MSELoss()
+optimizer = optim.Adam(net.parameters(), lr = 0.0001)
+
+# best loss
+best = 1e16
+num_bad_epochs = 0
+#can increase patience a lot, speed not an issue.
+patience = 10
+
+# %% Training
+# train
+loss_progress = np.empty(shape=(0,)) 
+num_bad_epochs = 0
+
+for epoch in range(10000): 
+    print("-----------------------------------------------------------------")
+    print("epoch: {}; bad epochs: {}".format(epoch, num_bad_epochs))
+    net.train()
+    running_loss = 0.
+
+    #tqdm shows a progress bar. 
+    for i, sim_E_vox_batch in enumerate(tqdm(trainloader), 0):
+        
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        pred_E_vox, pred_adc_prime, pred_adc, pred_sigma, pred_axr = net(sim_E_vox_batch)
+        
+        """print(sim_E_vox_batch)
+        print("pred_E_vox:", pred_E_vox)
+        print("pred_adc:", pred_adc)
+        print("pred_sigma:", pred_sigma)
+        print("pred_axr:", pred_axr)"""
+
+        if torch.isnan(pred_E_vox).any():
+            print("evox nan found")
+        if torch.isnan(pred_adc).any():
+            print("pred_adc nan found")
+        if torch.isnan(pred_axr).any():
+            print("pred_axr nan found")
+        if torch.isnan(pred_sigma).any():
+            print("sigpred_sigma nan found")
+            
+        #sim_E_vox_batch32 = sim_E_vox_batch.to(torch.float32)
+        #needed so that loss comparison works
+        
+        #print(pred_E_vox)
+        loss_sig = criterion(pred_E_vox, sim_E_vox_batch)
+        #loss_prime = criterion(pred_adc_prime,sim_adc_prime)
+        #loss prime needs sim_adc_prime to be batched. 
+
+        loss_sig.backward()
+        optimizer.step()
+        running_loss += loss_sig.item()
+        
+    print("loss: {}".format(running_loss))
+    # early stopping
+    if running_loss < best:
+        print("####################### saving good model #######################")
+        final_model = net.state_dict()
+        best = running_loss
+        num_bad_epochs = 0
+        loss_progress = np.append(loss_progress, best)
+    else:
+        num_bad_epochs = num_bad_epochs + 1
+        loss_progress = np.append(loss_progress, best)
+        if num_bad_epochs == patience:
+            print("done, best loss: {}".format(best))
+            break
+print("done")
+
+net.load_state_dict(final_model)
+
+net.eval()
+with torch.no_grad():
+    final_pred_E_vox, final_pred_adc_prime, final_pred_adc_repeated, final_pred_sigma_repeated, final_pred_axr_repeated = net(torch.from_numpy(sim_E_vox.astype(np.float32)))
+    # adc sigma and axr will have 8 columns which are all the same
+
+final_pred_adc = final_pred_adc_repeated[:, 0]
+final_pred_sigma = final_pred_sigma_repeated [:, 0]
+final_pred_axr = final_pred_axr_repeated[:, 0]
+
+
+# %% NN Plots
+
+plt.figure()
+plt.plot(range(1, len(loss_progress) + 1), loss_progress, marker='o', linestyle='-')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Loss per Epoch')
+plt.grid(True)
+plt.show()
+
+final_pred_E_vox_detached = final_pred_E_vox.detach().numpy()
+"""Was having numpy pytorch issues, so this line helps fix it a bit."""
+
+plt.scatter(be, sim_E_vox[0,:], label='simulated')
+plt.scatter(be, final_pred_E_vox_detached[0,:], label='predicted')
+plt.legend()
+
+# plot scatter plots to analyse correlation of predicted free params against ground truth
+plt.figure()
+
+param_sim = [sim_adc, sim_sigma, sim_axr]
+param_pred = [final_pred_adc, final_pred_sigma, final_pred_axr]
+param_name = ['ADC', 'Sigma', 'AXR']
+
+rvals = []
+
+for i,_ in enumerate(param_sim):
+    plt.rcParams['font.size'] = '16'
+    plt.scatter(param_sim[i], param_pred[i], s=2, c='navy')
+    plt.xlabel(param_name[i] + ' Ground Truth')
+    plt.ylabel(param_name[i] + ' Prediction')
+    rvals.append(scipy.stats.pearsonr(np.squeeze(param_sim[i]), np.squeeze(param_pred[i])))
+    plt.tight_layout
+    plt.show()
+
+print(rvals)
